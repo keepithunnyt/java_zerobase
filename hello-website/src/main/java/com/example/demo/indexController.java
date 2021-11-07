e7ca4e224e1398ff9b0d914f8bab8e7274ce2555
keepithunnyt <keepithunnyt@gmail.com>
2021-11-07 18:36:29 +0900

    1st

---
+++ hello-website/src/main/java/com/example/demo/indexController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class indexController {
+	
+	@GetMapping("/hello")
+	public String index() {
+		return "Hello";
+	}
+}
